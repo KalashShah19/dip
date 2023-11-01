9bc15afaf14a908f81984532c05a98eb003a9728
KalashShah19 <92210070+KalashShah19@users.noreply.github.com>
2023-11-01 14:06:49 +0530

    new

--- Practical List/p4.py
+++ Practical List/p4.py
@@ -1,27 +1,21 @@
+import cv2
+
 # Function to check pixel adjacency
-def check_adjacency(pixel1, pixel2, adjacency_type):
+def check_adjacency(image, pixel1, pixel2, adjacency_type):
     x1, y1 = pixel1
     x2, y2 = pixel2
 
-    dx = abs(x1 - x2)
-    dy = abs(y1 - y2)
-
     if adjacency_type == '4-adjacent':
-        return (dx == 1 and dy == 0) or (dx == 0 and dy == 1)
+        return abs(x1 - x2) + abs(y1 - y2) == 1
 
     if adjacency_type == '8-adjacent':
-        return dx <= 1 and dy <= 1
+        return abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1
 
     if adjacency_type == 'm-adjacent':
-        return dx <= 1 or dy <= 1
+        return abs(x1 - x2) <= 1 or abs(y1 - y2) <= 1
 
 # Input a grayscale image (you can load it with cv2.imread)
-# Assuming an image with a shape (height, width), where pixel positions are (x, y)
-image = [[0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0]]
+image = cv2.imread('gray_image.jpg', cv2.IMREAD_GRAYSCALE)
 
 # Input two pixel positions (x, y) for testing
 pixel1 = (1, 1)
@@ -31,7 +25,7 @@ pixel2 = (2, 1)
 adjacency_type = '4-adjacent'
 
 # Check adjacency and display the result
-result = check_adjacency(pixel1, pixel2, adjacency_type)
+result = check_adjacency(image, pixel1, pixel2, adjacency_type)
 
 if result:
     print(f"The pixels {pixel1} and {pixel2} are {adjacency_type}.")
